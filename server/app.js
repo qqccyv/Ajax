2c4c201ebbc0363fb73831cbec70f946498daab8
qqccyv <313187515@qq.com>
2020-01-26 17:55:20 +0800

    接收post请求参数

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
+
 const path = require('path');
 
 
-
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/first', (req, res) => {
@@ -14,6 +16,9 @@ app.get('/responseText', (req, res) => {
 })
 app.get('/get', (req, res) => {
     res.send(req.query)
+})
+app.post('/post', (req, res) => {
+    res.send(req.body)
 })
 app.listen('3000');
 console.log('服务器启动成功');
