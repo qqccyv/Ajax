da288c2d85a61a9a4dfe935c5f18166557d442ce
qqccyv <313187515@qq.com>
2020-01-28 21:52:52 +0800

    邮箱唯一性验证

---
+++ server/app1.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser')
+const fs = require('fs')
+const path = require('path');
+
+
+// app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.post('/first', (req, res) => {
+    res.status(400).send('hello Ajax')
+})
+app.post('/responseText', (req, res) => {
+    res.send({ "name": "dengyu" })
+})
+app.get('/get', (req, res) => {
+    res.send(req.query)
+})
+app.post('/post', (req, res) => {
+    res.send(req.body)
+})
+app.post('/json', (req, res) => {
+    res.send(req.body)
+})
+app.post('/readystate', (req, res) => {
+    res.send('hello state')
+})
+app.get('/error', (req, res) => {
+    res.status(400).send('not ok')
+})
+app.get('/cache', (req, res) => {
+    // fs.readFile('./test.txt', 'utf8', (err, data) => {
+    //     res.send(data)
+    // })
+    res.send(req.query)
+})
+app.listen('3000');
+console.log('服务器启动成功');
