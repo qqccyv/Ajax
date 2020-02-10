9c756a86b2fa538122f1da770b44698dc064f03c
qqccyv <313187515@qq.com>
2020-02-10 17:03:31 +0800

    ajax跨域方法

--- server/app1.js
+++ server/app1.js
@@ -8,7 +8,20 @@ const path = require('path');
 // app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
-
+    //!!!!!!!!!!!!!!!!!!!!!!!!!!!!重要
+    //ajax跨域方法
+    // 拦截所有请求
+app.use((req, res, next) => {
+    // 1.允许哪些客户端访问我
+    // * 代表允许所有的客户端访问我
+    // 注意：如果跨域请求中涉及到cookie信息传递，值不可以为*号 比如是具体的域名信息
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+        // 2.允许客户端使用哪些请求方法访问我
+    res.header('Access-Control-Allow-Methods', 'get,post')
+        // 允许客户端发送跨域请求时携带cookie信息
+    res.header('Access-Control-Allow-Credentials', true);
+    next();
+});
 app.post('/first', (req, res) => {
     res.status(400).send('hello Ajax')
 })
