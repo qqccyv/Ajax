1413efa1fa673342b57118707ce3295ba77c85fd
qqccyv <313187515@qq.com>
2020-02-15 16:01:32 +0800

    增加用户列表展示，用户编辑

--- alibaixiu1/alibaixiu/public/assets/js/users.js
+++ alibaixiu1/alibaixiu/public/assets/js/users.js
@@ -1,14 +1,70 @@
+jQuery || require('jquery')
+    //添加用户
 $('#addUser').on('submit', function() {
+        let formData = $(this).serialize();
+        $.ajax({
+            type: 'post',
+            url: '/users',
+            data: formData,
+            success: function() {
+                location.reload()
+            },
+            error: function() {
+                alert('用户添加失败')
+            }
+        })
+    })
+    //添加用户头像
+$('#userForm').on('change', '#avatar', function() {
+        let formData = new FormData();
+        formData.append('avatar', this.files[0])
+        $.ajax({
+            type: 'post',
+            url: '/upload',
+            data: formData,
+            processData: false,
+            contentType: false,
+            success: function(res) {
+                $('#preview').attr('src', res[0].avatar)
+                $('#hiddenAvatar').val(res[0].avatar)
+            }
+        })
+    })
+    //展示用户列表
+$.ajax({
+        type: 'get',
+        url: '/users',
+        success: function(res) {
+            let html = template('userTpl', {
+                users: res
+            })
+            $('#usersList').html(html)
+        }
+
+    })
+    //展示待修改用户信息
+$('#usersList').on('click', '.edit', function(e) {
+        e.preventDefault() //阻止默认行为
+        let id = $(this).data('id')
+        $.ajax({
+            type: 'get',
+            url: '/users/' + id,
+            success: function(res) {
+                let html = template('modifyTpl', res)
+                $('#userForm').html(html)
+            }
+        })
+    })
+    //修改用户信息
+$('#userForm').on('submit', '#modifyUser', function() {
+    let id = $(this).data('id')
     let formData = $(this).serialize();
     $.ajax({
-        type: 'post',
-        url: '/users',
+        type: 'put',
+        url: '/users/' + id,
         data: formData,
-        success: function() {
+        success: function(res) {
             location.reload()
-        },
-        error: function() {
-            alert('用户添加失败')
         }
     })
 })
