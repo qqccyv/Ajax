00b12d724c54f5c90f0a8c7cbfd03933f2b68320
qqccyv <313187515@qq.com>
2020-02-17 11:48:37 +0800

    添加编辑文章跳转与待编辑文章显示

--- alibaixiu1/alibaixiu/public/assets/js/post-add.js
+++ alibaixiu1/alibaixiu/public/assets/js/post-add.js
@@ -1,5 +1,6 @@
 jQuery || require('jquery')
-    //获取文章分类选择列表
+
+//获取文章分类选择列表
 $.ajax({
         type: 'get',
         url: '/categories',
@@ -49,3 +50,40 @@ $('#createPost').on('submit', function(e) {
     })
 
 })
+
+// 定义查询url参数的函数
+function getUrlParams(name) {
+    let paramsAray = location.search.substr(1).split('&')
+        // console.log(paramsAray);
+    for (let i = 0; i < paramsAray.length; i++) {
+        let temp = paramsAray[i].split('=')
+        if (temp[0] == name) {
+            return temp[1]
+        }
+    }
+    return -1
+}
+// 判断是否是修改页面
+let id = getUrlParams('id')
+if (id != -1) {
+    $.ajax({
+        type: 'get',
+        url: '/posts/' + id,
+        success: function(res) {
+            $.ajax({
+                type: 'get',
+                url: '/categories',
+                success: function(categories) {
+                    res.categories = categories
+                    let html = template('modifyTpl',
+                        res
+                    )
+                    console.log(res);
+                    console.log(html);
+
+                    $('#parentBox').html(html)
+                }
+            })
+        }
+    })
+}
